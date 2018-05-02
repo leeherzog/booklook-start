9e81530cfc779ef7193f6450ccefc108676f0a55
Ronit Herzog <lee.herzog5@gmail.com>
2018-05-02 12:30:10 +0300

    promises

--- script.js
+++ script.js
@@ -2,17 +2,17 @@ var fetch = function (code) {
     $.ajax({
       method: "GET",
       url: 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + code,
-      success: function(data) {
+    })
+    .then (function success (data) {
         console.log(data);
         var source = $("#book-template").html();
         var template = Handlebars.compile(source);
         var newHTML = template(data);
         $(".book").append(newHTML);
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
+      })
+    .catch (function error (jqXHR, textStatus, errorThrown) {
         console.log(textStatus);
-      }
-    }); 
+      });
   };
 
   $(".search-book").on("click", function(){
